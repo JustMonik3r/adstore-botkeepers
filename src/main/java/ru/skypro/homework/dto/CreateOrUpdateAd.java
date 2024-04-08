f4717e29894ab7725e988bebf8933df24eecb609
JustMonik3r <m.mintous@gmail.com>
2024-04-08 18:57:33 +0300

    added dto for:
    Ads
    CreateOrUpdateAd
    CreateOrUpdateComment
    NewPassword

--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateAd.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAd.java
@@ -1,4 +1,17 @@
 package ru.skypro.homework.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateOrUpdateAd {
+
+    private String title;
+
+    private Integer price;
+
+    private String description;
+
 }
--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateComment.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateComment.java
@@ -1,4 +1,19 @@
 package ru.skypro.homework.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateOrUpdateComment {
+
+    private Integer pk;
+
+    private String text;
+
+    private LocalDateTime createdAt;
+
 }
