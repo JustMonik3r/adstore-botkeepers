88636f3abeea7cb605e2f1d2eb8ee89291f13192
iShantaram <1agapovav987@gmail.com>
2024-04-09 19:30:55 +0300

    Added Ad-dto properties, ExtendedAd-dto properties and Ad-entity.

--- src/main/java/ru/skypro/homework/entity/Ad.java
+++ src/main/java/ru/skypro/homework/entity/Ad.java
@@ -1,4 +1,34 @@
 package ru.skypro.homework.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ads")
 public class Ad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pk;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "price", nullable = false)
+    private Integer price;
+    @Column(name = "image")
+    private String imageUrl;
+    @Column(name = "data")
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] data;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
